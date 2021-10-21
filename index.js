868927335ea3b2e7c4a1f3880d1c191b89eaca85
luirusso <luirusso_@hotmail.it>
2021-10-21 16:07:50 +0200

    added adult and over 65 conditions

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 - va applicato uno sconto del 40% per gli over 65.
 L’output del prezzo finale va scritto in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
 
+const askKm = prompt("Quanti KM dovrai percorrere?");
+console.log(`KM da percorrere:`, askKm);
+
+const askAge = prompt("Quanti anni hai?");
+console.log(`Età:`, askAge);
+
+if ( askAge < 18 ) {
+    console.log(`Utente è minorenne`);
+}
+else if ( askAge > 65 ) {
+    console.log(`Utente è over 65`)
+}
+else if ( askAge >= 18 ) {
+    console.log(`Utente è maggiorenne`);
+}
+
